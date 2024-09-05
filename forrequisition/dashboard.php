28f7d32182e8e2944fb28eb9648653999d7a4380
manishb70 <mb8882568627@gmail.com>
2024-09-05 21:11:31 +0530

    Updated success

--- forrequisition/dashboard.php
+++ forrequisition/dashboard.php
@@ -94,15 +94,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             <?php
 
             // if ($_SESSION["role"] == "requisitionGen" || $role == "admin") {
-            if ($_SESSION["store_genrate"] ||  ($_SESSION["store_genrate"]   || $role == "admin") {
+            if ($_SESSION["store_genrate"] || $role == "admin") {
 
                 ?>
-
-
-
-
-
-
 
                 <form action="" method="post">
                     <button type="submit"
@@ -199,17 +193,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 class="py-2.5 px-5 me-2 mb-2 text-sm  font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                 User Master Creation Form </a>
 
-
-              
-         
-
+                            
 
             <a type="submit" href="./createPo.php"
                 class="py-2.5 px-5 me-2 mb-2 text-sm  font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                 Purchase Order Creation Form </a>
 
 
-                        
+
             <a type="submit" href="./CustomerForm.php"
                 class="py-2.5 px-5 me-2 mb-2 text-sm  font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                 Customer Creation Form </a>
